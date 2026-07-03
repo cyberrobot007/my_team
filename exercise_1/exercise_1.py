2e7467a8372a05b65d77d18d4be0e7d482d20ce1
cyberrobot <cyberrobot007@gmail.com>
2026-07-03 20:17:51 +0700

    add to card exercise 2

--- exercise_1/exercise_1.py
+++ exercise_1/exercise_1.py
@@ -1,3 +1,5 @@
+#excercise 1
+
 item_name = "shampoo"
 item_price = 15
 is_in_stock = True
@@ -5,3 +7,11 @@ is_in_stock = True
 is_in_stock = "out of stock"
 
 print(is_in_stock)
+
+#excercise 2
+
+user_age = "26"
+age_in_next = int(user_age)
+next_year = age_in_next + 4
+print(next_year)
+
